bc1bb8d603b9ed2cff031d8024b5f7b16ddcb2f8
Francesco <7frapar@gmail.com>
2022-07-02 17:25:38 +0200

    prime funzioni

--- main.hs
+++ main.hs
@@ -1,2 +1,27 @@
 -- Modular Strings, Riccardo Pratesi
 -- Script edited by Francesco Parisio
+
+zeros :: Int -> [Int]
+zeros n = replicate n 0
+
+primordial n = zeros (n-1) ++ [1]
+
+type Modular = [Int]
+type Evolutions = [Modular]
+
+compute :: Int -> Int -> Int -> Int
+compute a b n = (a+b) `mod` n
+
+evolve :: Modular -> Int -> Int -> Modular
+evolve (x:xs) modulo first =
+  if null xs then
+    [compute x first modulo]
+  else
+    compute x (head xs) modulo : evolve xs modulo first
+
+-- Returns an Infinite list of evolutions!
+evolution :: Modular -> Evolutions
+evolution m = m : evolution ( evolve m (length m) (head m) )
+
+evolutionsOf :: Int -> Int -> Evolutions
+evolutionsOf k m = take k $ evolution $ primordial m
